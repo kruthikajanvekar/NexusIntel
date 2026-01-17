10fe5f8986a11537e1c5462151e292c06f6adc78
kruthikajanvekar <kruthika7824@gmail.com>
2026-01-17 13:07:41 +0530

    Add ORM configuration and database connection logic

---
+++ src/database/orm.py
@@ -0,0 +1,5 @@
+
+# DEPRECATED: Redundant file. 
+# All Database/ORM logic is now consolidated in src/database/models.py
+from .models import save_intel as save_to_db
+from .models import get_history as get_all_history
